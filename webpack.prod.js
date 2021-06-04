8c8da2df41951d3a186eae797b69a8bbc79aa64a
saurabhshankariya <saurabhshankariya@advance-auto.com>
2021-06-04 23:19:45 +0530

    Webpack prod config updates

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -29,14 +31,26 @@ module.exports = merge(common, {
             },
         },
         minimizer: [
-            new OptimizeCssAssetsPlugin({
-                cssProcessorOptions: {
-                    map: {
-                        inline: false,
-                        annotation: true,
-                    },
-                },
+            new OptimizeCssAssetsPlugin(),
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+                uglifyOptions: {
+                    compress: {
+                        inline: false
+                    }
+                }
             })
         ]
     },
+    plugins: [
+        new CompressionPlugin({
+            test: /\.js(\?.*)?$/i,
+            minRatio: 0.8,
+            threshold: 8192,
+            algorithm: "gzip",
+            exclude: /.map$/
+        }),
+    ],
 });
